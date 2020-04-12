82db62bd1af89c2b9f3f2a233c59889ddc7a3c27
Artem Yudin <yudin.artem.90@yandex.ru>
2020-04-12 23:53:58 +0300

    fix

--- script/index.js
+++ script/index.js
@@ -1,26 +1,13 @@
 const buttonOpenModalEditProfileInfo = document.querySelector('.profile-info__edit-button');
 const buttonCloseModalEditProfileInfo = document.querySelector('.popup__icon');
-const saveDataProfile = document.querySelector('.popup__save-button');
+const buttonSaveDataProfile = document.querySelector('.popup__save-button');
 
 
 const closeModalEditProfileInfo = function(){
     document.querySelector('.popup').classList.remove('popup_opened');
 };
 
-buttonCloseModalEditProfileInfo.addEventListener('click', closeModalEditProfileInfo);
-
-buttonOpenModalEditProfileInfo.addEventListener('click', function(){
-    const name = document.querySelector('.profile-info__name');
-    const informPerson = document.querySelector('.profile-info__information-person');
-    let popupField = document.querySelectorAll('.popup__field');
-
-    popupField[0].value = name.textContent;
-    popupField[1].value = informPerson.textContent;
-
-    document.querySelector('.popup').classList.add('popup_opened');
-});
-
-saveDataProfile.addEventListener('click', function(){
+const saveDataProfile = function(){
     const popupField = document.querySelectorAll('.popup__field');
     const name = popupField[0].value;
     const informPerson = popupField[1].value;
@@ -32,4 +19,26 @@ saveDataProfile.addEventListener('click', function(){
     informPage.textContent = informPerson;
     
     closeModalEditProfileInfo();
+}
+
+buttonCloseModalEditProfileInfo.addEventListener('click', closeModalEditProfileInfo);
+buttonSaveDataProfile.addEventListener('click', saveDataProfile);
+
+document.addEventListener('keypress', function(e){
+    const activPopup = document.querySelector('.popup_opened');
+    if(e.key === 'Enter' && activPopup !== null){
+        saveDataProfile();
+    }
+});
+
+
+buttonOpenModalEditProfileInfo.addEventListener('click', function(){
+    const name = document.querySelector('.profile-info__name');
+    const informPerson = document.querySelector('.profile-info__information-person');
+    let popupField = document.querySelectorAll('.popup__field');
+
+    popupField[0].value = name.textContent;
+    popupField[1].value = informPerson.textContent;
+
+    document.querySelector('.popup').classList.add('popup_opened');
 });
