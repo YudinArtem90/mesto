78a265114d8e1361d89564378ca622116c6d9d3d
Artem Yudin <yudin.artem.90@yandex.ru>
2020-04-15 18:59:13 +0300

    fix js

--- script/index.js
+++ script/index.js
@@ -1,19 +1,20 @@
 const buttonOpenModalEditProfileInfo = document.querySelector('.profile-info__edit-button');
 const buttonCloseModalEditProfileInfo = document.querySelector('.popup__icon');
 const buttonSaveDataProfile = document.querySelector('.popup__save-button');
+const informPage = document.querySelector('.profile-info__information-person');
+const activPopup = document.querySelector('.popup_opened');
+const namePage = document.querySelector('.profile-info__name');
+const popup = document.querySelector('.popup');
+const popupField = document.querySelectorAll('.popup__field');
 
 
 const closeModalEditProfileInfo = function(){
-    document.querySelector('.popup').classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
 };
 
 const saveDataProfile = function(){
-    const popupField = document.querySelectorAll('.popup__field');
     const name = popupField[0].value;
     const informPerson = popupField[1].value;
-
-    const namePage = document.querySelector('.profile-info__name');
-    const informPage = document.querySelector('.profile-info__information-person');
     
     namePage.textContent = name;
     informPage.textContent = informPerson;
@@ -24,21 +25,17 @@ const saveDataProfile = function(){
 buttonCloseModalEditProfileInfo.addEventListener('click', closeModalEditProfileInfo);
 buttonSaveDataProfile.addEventListener('click', saveDataProfile);
 
-document.addEventListener('keypress', function(e){
-    const activPopup = document.querySelector('.popup_opened');
-    if(e.key === 'Enter' && activPopup !== null){
+popup.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
         saveDataProfile();
     }
 });
 
 
 buttonOpenModalEditProfileInfo.addEventListener('click', function(){
-    const name = document.querySelector('.profile-info__name');
-    const informPerson = document.querySelector('.profile-info__information-person');
-    let popupField = document.querySelectorAll('.popup__field');
 
-    popupField[0].value = name.textContent;
-    popupField[1].value = informPerson.textContent;
+    popupField[0].value = namePage.textContent;
+    popupField[1].value = informPage.textContent;
 
-    document.querySelector('.popup').classList.add('popup_opened');
+    popup.classList.add('popup_opened');
 });
