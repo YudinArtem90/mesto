af33b9c5131b9fa51b9edc3d16048f91e4f90bfa
Artem Yudin <yudin.artem.90@yandex.ru>
2020-06-16 21:26:45 +0300

    настроил webpack + добавил gitignore

---
+++ src/Card.js
@@ -0,0 +1,53 @@
+import {
+    containerPopupViewPhoto,
+    infoPopupViewPhoto,
+    popupViewPhoto,
+    openAndClosePopup
+} from './index.js';
+
+export default class Card{
+
+    constructor(data, template){
+        this._name = data.name;
+        this._link = data.link;
+        this._templateCard = template;
+    }
+
+    _addLikeOrDislikeCard() {
+        this._buttonCardLike.classList.toggle("element__button-like_action");
+    }
+
+    _createPopupViewPhoto(event){
+        containerPopupViewPhoto.src = event.target.currentSrc; 
+        containerPopupViewPhoto.setAttribute('alt', `Фото - ${this._name}`);
+        infoPopupViewPhoto.textContent = this._name;
+        openAndClosePopup(popupViewPhoto, event);
+    }
+
+    _deleteCard(){
+        this._element.remove();
+        this._element = null;
+    }
+
+    getCard (){
+        this._element = this._templateCard.firstElementChild.cloneNode(true);
+        this._buttonDeleteCard = this._element.querySelector('.element__button-delete');
+        this._buttonCardLike = this._element.querySelector('.element__button-like');
+        this._image =  this._element.querySelector('.element__image');
+        const text = this._element.querySelector('.element__text');
+    
+        this._image.src = this._link;
+        this._image.setAttribute('alt', this._name);
+        text.textContent = this._name;
+
+        this._addEventListenerCard();
+
+        return this._element;
+    }
+
+    _addEventListenerCard(){
+        this._buttonCardLike.addEventListener('click', this._addLikeOrDislikeCard);
+        this._image.addEventListener('click', this._createPopupViewPhoto);
+        this._buttonDeleteCard.addEventListener('click', () => this._deleteCard(), {onсe : true});
+    }
+}
---
+++ src/FormValidator.js
@@ -0,0 +1,74 @@
+import {
+    buttonDisabled, 
+    buttonActivated, 
+    findInputsForm, 
+    findSpanError,
+    showInputError,
+    hideInputError
+} from './index.js';
+
+export default class FormValidator{
+    constructor(pageElements, validateForm = 0){
+        this._pageElements = pageElements;
+        this._validateForm = validateForm;
+    }
+
+    enableValidation(){
+        const forms = document.querySelectorAll(this._pageElements.form);
+
+        forms.forEach((form) => {
+            const inputsForm = findInputsForm(form, this._pageElements); 
+
+            this._preventDefaultForm(form);
+            
+            this._addEventListenerInputForm(inputsForm, form);
+            
+        });
+    }
+
+    _addEventListenerInputForm (inputsForm, form) {
+        inputsForm.forEach((input) => {
+            input.addEventListener('input', () => this._validationInputAndForm(inputsForm, form, input));
+        });
+    }
+
+    _validationInputAndForm(inputsForm, form, input = 0){
+        if(input){
+            this._validInput(input, form);
+        }else{
+            inputsForm.forEach((inputForm) => {
+                this._validInput(inputForm, this._validateForm);
+            });
+        }
+        
+        this._validForm(inputsForm, form);
+    }
+
+    _preventDefaultForm (form) {
+        form.addEventListener('submit', function (evt) {
+          evt.preventDefault();
+        });
+    }
+
+    _validInput(formInput, form) {
+        const spanError = findSpanError(formInput, form);
+      
+        formInput.validity.valid ?
+                hideInputError(spanError, formInput) :
+                showInputError(spanError, formInput);
+    }
+
+    _isFormInvalid(formInputs) {
+        return Array.from(formInputs).some((input) => {
+          return !input.validity.valid;
+        });
+    }
+
+    _validForm(formInputs, form){
+        const buttonSave = form.querySelector(this._pageElements.buttonSaveForm);
+      
+        this._isFormInvalid(formInputs) ? 
+                buttonDisabled(buttonSave) : 
+                buttonActivated(buttonSave);
+    }
+}
