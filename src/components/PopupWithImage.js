ba2b337062f12834f37b048f0a31720ccf1a71d1
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2020-06-21 14:09:09 +0300

    исправил PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,17 +1,20 @@
 import Popup from "./Popup";
-import {infoPopupViewPhoto, containerPopupViewPhoto} from '../pages/index';
 
 export default class PopupWithImage extends Popup{
-    constructor(src, name, selector){
-        super(selector);
+    constructor(src, name, pageElements){
+        const {popupViewPhoto, buttonClosePopup, containerPopupViewPhoto, infoPopupViewPhoto} = pageElements;
+        super(popupViewPhoto, buttonClosePopup);
+
+        this._popupViewPhoto = document.querySelector(containerPopupViewPhoto);
+        this._infoPopup = document.querySelector(infoPopupViewPhoto);
         this._src = src;
         this._name = name;
     }
 
     open() {
-        containerPopupViewPhoto.src = this._src; 
-        containerPopupViewPhoto.setAttribute('alt', `Фото - ${this._name}`);
-        infoPopupViewPhoto.textContent = this._name;
+        this._popupViewPhoto.src = this._src; 
+        this._popupViewPhoto.setAttribute('alt', `Фото - ${this._name}`);
+        this._infoPopup.textContent = this._name;
         super.setEventListeners();
         super.open();
     }
