38c0f6f158a47ba33f3f2dab7f6d31de74c836ee
artemYudin041290 <artem.yudin@mediadirectiongroup.ru>
2020-06-29 16:02:30 +0300

    перекинул ajax в отдельный каталог

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Section from '../components/Section.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
 import PopupWithImage from '../components/PopupWithImage.js';
-
+import ajax from '../utils/ajax';
 import './index.css';
 
 /**
@@ -38,46 +38,6 @@ const validateFormAddCard = new FormValidator(pageElements, formCardAdd);
 const userInfo = new UserInfo(pageElements);
 
 const createPopupViewPhoto = new PopupWithImage(pageElements);
-
-const ajax = (url, method = "GET", body, contentType) => {
-
-    const data = {
-        method: method,
-        headers: {
-            authorization: 'f77ffc2a-fabb-4e1a-b96f-391d240718e4'
-        }
-    };
-
-    if(body){
-        data.body = JSON.stringify(body);
-    }
-
-    if(contentType){
-        data['headers']['Content-Type'] = contentType;
-    }
-
-    return new Promise(function(resolve) {
-        fetch(url, data)
-        .then(res => {
-            if(res.ok){
-                return res.json();
-            }
-            return Promise.reject(`Ошибка: ${res.status}`);
-            })
-        .then((result) => {
-            console.log('result', result);
-            let j = result;
-            if(url === 'https://mesto.nomoreparties.co/v1/cohort-12/cards'){
-             j = result.slice(1, 3);
-            }
-            // let j = result.slice(1, 3);
-            // console.log(j);
-            resolve(j);
-        })
-        .catch((error) => console.log(error))
-        .finally((result) => console.log(result))
-    })
-}
 
 const popupEditProfileForm = new PopupWithForm(
     pageElements.popupEditProfile, 
---
+++ src/utils/ajax.js
@@ -0,0 +1,41 @@
+const ajax = (url, method = "GET", body, contentType) => {
+
+    const data = {
+        method: method,
+        headers: {
+            authorization: 'f77ffc2a-fabb-4e1a-b96f-391d240718e4'
+        }
+    };
+
+    if(body){
+        data.body = JSON.stringify(body);
+    }
+
+    if(contentType){
+        data['headers']['Content-Type'] = contentType;
+    }
+
+    return new Promise(function(resolve) {
+        fetch(url, data)
+        .then(res => {
+            if(res.ok){
+                return res.json();
+            }
+            return Promise.reject(`Ошибка: ${res.status}`);
+            })
+        .then((result) => {
+            console.log('result', result);
+            let j = result;
+            if(url === 'https://mesto.nomoreparties.co/v1/cohort-12/cards'){
+             j = result.slice(1, 3);
+            }
+            // let j = result.slice(1, 3);
+            // console.log(j);
+            resolve(j);
+        })
+        .catch((error) => console.log(error))
+        .finally((result) => console.log(result))
+    })
+}
+
+export default ajax;
